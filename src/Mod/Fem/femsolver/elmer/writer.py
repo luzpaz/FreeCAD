cace5d1bc00a3e90c8ab77c80de0f2d7c5d0723b
luz.paz <luzpaz@users.noreply.github.com>
2018-11-01 17:32:42 -0400

    FEM: solver, make file header title use the same schema in all solver modules
    Made uniform all __title__ doxy \addtogroup blocks

--- src/Mod/Fem/femsolver/elmer/writer.py
+++ src/Mod/Fem/femsolver/elmer/writer.py
@@ -20,11 +20,12 @@
 # *                                                                         *
 # ***************************************************************************
 
-
-__title__ = "FemWriterElmer"
+__title__ = "FreeCAD FEM solver Elmer writer"
 __author__ = "Markus Hovorka"
 __url__ = "http://www.freecadweb.org"
 
+## \addtogroup FEM
+#  @{
 
 import os
 import os.path
@@ -780,3 +781,5 @@ class Writer(object):
 
 class WriteError(Exception):
     pass
+
+##  @}

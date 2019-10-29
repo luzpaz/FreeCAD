1a720eb4b9f0b1a0b90657ec063dff8a7c343f66
luz.paz <luzpaz@users.noreply.github.com>
2019-10-29 06:56:10 -0400

    Test WB: In app startup dialog explaining the workbench
    Code lifted from BIM WB's dialogWelcome.ui

---
+++ src/Mod/Test/TestWelcome.py
@@ -0,0 +1,72 @@
+#***************************************************************************
+#*                                                                         *
+#*   Copyright (c) 2017 Yorik van Havre <yorik@uncreated.net>              *
+#*                                                                         *
+#*   This program is free software; you can redistribute it and/or modify  *
+#*   it under the terms of the GNU Lesser General Public License (LGPL)    *
+#*   as published by the Free Software Foundation; either version 2 of     *
+#*   the License, or (at your option) any later version.                   *
+#*   for detail see the LICENCE text file.                                 *
+#*                                                                         *
+#*   This program is distributed in the hope that it will be useful,       *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+#*   GNU Library General Public License for more details.                  *
+#*                                                                         *
+#*   You should have received a copy of the GNU Library General Public     *
+#*   License along with this program; if not, write to the Free Software   *
+#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+#*   USA                                                                   *
+#*                                                                         *
+#***************************************************************************
+
+"""
+This module contains FreeCAD commands for the Test workbench.
+Upon opening the Test workbench for the first time, a dialog box will appear
+to inform the user about the functionality of the Test workbench.
+"""
+
+import os,FreeCAD,FreeCADGui
+
+from PySide import QtCore,QtGui
+
+def QT_TRANSLATE_NOOP(ctx,txt): return txt # dummy function for the QT translator
+
+
+
+class Test_Welcome:
+
+
+    def GetResources(self):
+
+        return {'MenuText': QT_TRANSLATE_NOOP("Test_Welcome", "Test WB Welcome screen"),
+                'ToolTip' : QT_TRANSLATE_NOOP("Test_Welcome", "Show the Test workbench welcome screen")}
+
+    def Activated(self):
+
+        # load dialog
+        self.form = FreeCADGui.PySideUic.loadUi(os.path.join(os.path.dirname(__file__),"dialogWelcome.ui"))
+
+        # set the title image
+        # self.form.image.setPixmap(QtGui.QPixmap(os.path.join(os.path.dirname(__file__),"icons","banner.png")))
+
+        # handle the tutorial link
+        # QtCore.QObject.connect(self.form.label_4, QtCore.SIGNAL("linkActivated(QString)"), self.launchTutorial)
+
+        # center the dialog over FreeCAD window
+        mw = FreeCADGui.getMainWindow()
+        self.form.move(mw.frameGeometry().topLeft() + mw.rect().center() - self.form.rect().center())
+
+        # show dialog and run setup dialog afterwards if OK was pressed
+        result = self.form.exec_()
+        # if result:
+        #    FreeCADGui.runCommand("BIM_Setup")
+            
+        # remove first time flag
+        FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Test").SetBool("FirstTime",False)
+
+    # def launchTutorial(self,link):
+    # 
+    #     if hasattr(self,"form"):
+    #         self.form.hide()
+    #         FreeCADGui.runCommand("BIM_Tutorial")

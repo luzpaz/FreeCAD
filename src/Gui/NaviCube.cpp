6ce24c767353dc11d2e4397e0b2311bdd6ff00aa
luzpaz <luzpaz@users.noreply.github.com>
2023-01-27 20:11:44 +0000

    Gui: fix trailing whitespace

--- src/Gui/NaviCube.cpp
+++ src/Gui/NaviCube.cpp
@@ -273,8 +273,8 @@ QFont NaviCube::getDefaultSansserifFont()
     // on Windows 10 or newer there is no Helvetia font
     // therefore if we did not found a Helvetica font check for
     // first the Bahnschrift font (in all Windows since 2017
-    // if this is also not found (on Win 7), we check for the 
-    // the DejaVu Sans fonts
+    // if this is also not found (on Win 7), we check for the
+    // DejaVu Sans fonts
     font.setFamily(QString::fromLatin1("Bahnschrift"));
     // on Windows 11 sansserif fonts like Bahnschrift do not have the
     // styleHint QFont::SansSerif but QFont::AnyStyle
@@ -328,7 +328,7 @@ void NaviCubeImplementation::OnChange(ParameterGrp::SubjectType& rCaller,
                                       ParameterGrp::MessageType reason)
 {
     const auto& rGrp = static_cast<ParameterGrp&>(rCaller);
-    
+
     if (strcmp(reason, "TextColor") == 0) {
         m_TextColor.setRgba(rGrp.GetUnsigned(reason, QColor(0, 0, 0, 255).rgba()));
     }
@@ -424,7 +424,7 @@ auto convertWeights = [](int weight) -> QFont::Weight {
 
 GLuint NaviCubeImplementation::createCubeFaceTex(QtGLWidget* gl, float gap, const char* text,
                                                  int shape)
-{  
+{
     int texSize = m_CubeWidgetSize * m_OverSample;
     float gapi = texSize * gap;
     QImage image(texSize, texSize, QImage::Format_ARGB32);
